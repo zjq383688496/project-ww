48591443186d531dc47ea01e21c2da07de27b65e
jimmy.zhuang <jimmy.zhuang@weimob.com>
2017-08-21 11:08:46 +0800

    Update

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ const http    = require('http')
 app.disable('x-powered-by')
 app.use(express.static(__dirname + '/src'))
 
+app.use((req, res, next) => {
+	res.status(404).send('404')
+})
+app.use((err, req, res, next) => {
+	res.status(500).send('500')
+})
+
 app.listen(3000)
