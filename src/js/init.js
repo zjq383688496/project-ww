6b1e8e3c8cf0915d9c2c428a78475eb829b5ed59
zjq383688496 <zjq383688496@gmail.com>
2017-08-28 02:52:50 +0800

    no message

--- src/js/init.js
+++ src/js/init.js
@@ -3,8 +3,11 @@
 if (window.API && window.CONFIG) return
 
 window.API = {
-	login: 'open/login.html',		// 登录
-	reg:   'open/register.html' 	// 注册
+	login:   'open/login.html',		// 登录
+	reg:     'open/register.html', 	// 注册
+	balance: 'open/balance.html',	// 余额
+	room:    'open/getRooms.html',	// 房间
+	prorate: 'open/prorate.html',	// 资产赔率
 }
 
 window.CONFIG = {
