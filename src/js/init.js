f6953ffb9e7a1b20b1019ff66f694e56d027544b
jimmy.zhuang <jimmy.zhuang@weimob.com>
2017-09-16 19:06:11 +0800

    Update

--- src/js/init.js
+++ src/js/init.js
@@ -3,12 +3,13 @@
 if (window.API && window.CONFIG) return
 
 window.API = {
-	login:   'open/login.html',		// 登录
-	reg:     'open/register.html', 	// 注册
-	balance: 'open/balance.html',	// 余额
-	// room:    'open/getRooms.html',	// 房间
-	prorate: 'open/prorate.html',	// 资产赔率
-	room:    'open/get.html',
+	login:    'open/login.html',		// 登录
+	reg:      'open/register.html', 	// 注册
+	balance:  'open/balance.html',		// 余额
+	prorate:  'open/prorate.html',		// 资产赔率
+	room:     'open/get.html',
+	getUsers: 'open/getUsers.html',
+	todayProfit:          'open/todayProfit.html',
 	getLatestRoundDetail: 'open/getLatestRoundDetail.html'
 }
 
